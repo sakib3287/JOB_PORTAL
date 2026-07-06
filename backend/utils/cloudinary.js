e25275c3f3565269612894308bdfe74e893d694c
sakib3287 <sakibmalik328725@gmail.com>
2026-07-06 11:12:48 +0530

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -2,6 +2,13 @@ import {v2 as cloudinary} from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config();
 
+
+console.log({
+    cloud: process.env.CLOUD_NAME,
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET ? "Loaded" : "Missing"
+});
+
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY,
